9f89d1ea2220d70ab03cb63298ad95c202c8f949
daniel-needham <danielneedhamdn@gmail.com>
2024-04-10 14:41:01 +0100

    Updates

---
+++ _app/immutable/chunks/paths.79852687.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_13fv2ya)==null?void 0:s.base)??"/storygen";var a;const t=((a=globalThis.__sveltekit_13fv2ya)==null?void 0:a.assets)??e;export{t as a,e as b};
